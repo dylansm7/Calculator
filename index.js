d13f2293bdde6c41cf901598de074f93888a206d
Solis Murillo <solisd4@emea.roche.com>
2024-02-28 23:45:03 +0100

    changes

---
+++ index.js
@@ -0,0 +1,13 @@
+let operationDisplay = document.querySelector('#operation');
+let resultDisplay = document.querySelector('#operation-result');
+let buttons = document.querySelector('.btn');
+
+
+operationDisplay.value = '';
+resultDisplay.value = '';
+
+
+
+buttons.addEventListener('onclick', () => {
+  console.log('Hello');
+})
